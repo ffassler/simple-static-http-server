a31a22c354f829351d76b170c6e094afe9186591
Fabien Fassler <fabien.fassler@sqits.net>
2019-04-25 15:11:42 +0200

    Error management (#1)
    * Add exit codes
    
    * Add testing

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM golang:1.11 as builder
 COPY main.go .
+COPY main_test.go .
 RUN GOOS=linux go build -o simple-http-server
+RUN GOOS=linux go get github.com/stretchr/testify/assert
+RUN GOOS=linux go test
 
 FROM alpine:3.9
 RUN apk add --no-cache libc6-compat
--- main.go
+++ main.go
@@ -8,21 +8,42 @@ import (
 	"os"
 )
 
+const DefaultPort = 8080
+
+const (
+	CurrentDirectoryErrorExitCode = 1
+	ListenAndServeErrorExitCode   = 2
+)
+
 func main() {
 
 	currentDirectory, err := os.Getwd()
 	if err != nil {
 		log.Fatal("Cannont get current directory : ", err)
+		os.Exit(CurrentDirectoryErrorExitCode)
 	}
 
 	directory := flag.String("d", currentDirectory, "Directory which contains static files to be served")
-	port := flag.Int("p", 8080, "Port")
+	port := flag.Int("p", DefaultPort, "Port")
 
 	flag.Parse()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, fmt.Sprint(*directory, "/", r.URL.Path[1:]))
+		http.ServeFile(w, r, GetServeFilename(*directory, r.URL.Path))
 	})
 
-	log.Fatal("Cannot start the http server : ", http.ListenAndServe(fmt.Sprint(":", *port), nil))
+	errListenAndServe := http.ListenAndServe(fmt.Sprint(":", *port), nil)
+
+	if errListenAndServe != nil {
+		log.Fatal("Cannot start the http server : ", errListenAndServe)
+		os.Exit(ListenAndServeErrorExitCode)
+	}
+}
+
+func GetServeFilename(directory string, path string) string {
+	if len(path) > 0 {
+		path = path[1:]
+	}
+
+	return fmt.Sprint(directory, "/", path)
 }
